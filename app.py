64f68ee8651b3b3afca5f54785506c7e91ca70b2
manc1n1 <mancinij1111@gmail.com>
2024-04-10 20:15:26 -0400

    Add API link

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from dash import Dash, html
+import requests
 
 app = Dash(__name__)
 
-app.layout = html.Div([html.Div(children="Hello World")])
+app.layout = html.Div(children=[html.H1(id="title", children="News Channel 1")])
+
+location = ""
+url = "https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language=en&format=json".format(
+    location
+)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
